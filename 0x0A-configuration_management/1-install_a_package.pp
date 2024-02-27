045a866d3d6e52333e127fbf737e346d95718be1
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-27 00:28:13 +0000

    the implementations

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using Puppet to install flask from pip3 with the requiremnts in there
 package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
