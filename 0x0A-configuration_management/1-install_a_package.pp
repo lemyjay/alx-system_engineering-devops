1feab63095befc19a6b4ecb6c174b94e317d34e5
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-27 00:37:50 +0000

    the implementations

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Install flask using puppet
-
+# Install Flask version 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Install the latest version of Werkzeug
+package { 'werkzeug':
+  ensure   => 'latest',
+  provider => 'pip3',
 }
