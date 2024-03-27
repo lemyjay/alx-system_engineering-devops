f004f5c97ecb57c75804d32dd47b8fbdb5185121
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-27 14:31:08 +0000

    the implementation of task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+Python script that, using this REST API
+`https://jsonplaceholder.typicode.com/todos/`, for a given employee ID,
+returns information about his/her TODO list progress.
+'''
+from sys import argv, exit
+import requests
+
+if __name__ == "__main__":
+    if len(argv) != 2 or not argv[1].isdigit():
+        exit(1)
+    try:
+        EMPLOYEE_ID = int(argv[1])
+        BASE_URL = 'https://jsonplaceholder.typicode.com/'
+        USER_URL = f"{BASE_URL}users/{EMPLOYEE_ID}"
+        TODOS_URL = f"{BASE_URL}/todos?userId={EMPLOYEE_ID}"
+        EMPLOYEE_NAME = requests.get(USER_URL).json()["name"]
+        NUMBER_OF_DONE_TASKS = 0
+        TOTAL_NUMBER_OF_TASKS = len(requests.get(TODOS_URL).json())
+
+        for i in requests.get(TODOS_URL).json():
+            if i['completed'] is True:
+                NUMBER_OF_DONE_TASKS += 1
+
+        print(f"Employee {EMPLOYEE_NAME} is done with "
+              f"tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
+        for i in requests.get(TODOS_URL).json():
+            if i['completed'] is True:
+                print("\t " + i['title'])
+    except e as Exception:
+        pass
