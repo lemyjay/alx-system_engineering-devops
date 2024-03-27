ff30bc8aa65e186dcd83c768c3f1e0d6b015eb71
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-27 14:32:57 +0000

    alphabetically ordering the import libraries

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,8 @@ Python script that, using this REST API
 `https://jsonplaceholder.typicode.com/todos/`, for a given employee ID,
 returns information about his/her TODO list progress.
 '''
-from sys import argv, exit
 import requests
+from sys import argv, exit
 
 if __name__ == "__main__":
     if len(argv) != 2 or not argv[1].isdigit():
