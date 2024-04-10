952669e6f604f3031f4cb7ff9b22ed567cdc493d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-04-10 00:29:36 +0000

    some minor changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,20 +4,24 @@ A script that contains a function that queries the Reddit API and returns the
 number of subscribers (not active users, total subscribers) for
 a given subreddit.
 '''
-import requests
 
 
-def number_of_subscribers(subreddit):
-    '''
-    Returns the number of subcribers for a given valid subreddit or 0 if
-    subreddit is invalid
-    '''
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+if __name__ != '__main__':
+    '''Run only if the file is imported'''
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
-        return 0
+    def number_of_subscribers(subreddit):
+        '''
+        Returns the number of subcribers for a given valid subreddit or 0 if
+        subreddit is invalid
+        '''
+        import requests
+
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        headers = {'User-Agent': 'Custom User Agent'}
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
