6fdfaca1c258aa1a3b4b579caf7f199120aba7a2
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-04-10 00:19:13 +0000

    correction to the shebang of task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 '''
 A script that contains a function that queries the Reddit API and returns the
 number of subscribers (not active users, total subscribers) for
