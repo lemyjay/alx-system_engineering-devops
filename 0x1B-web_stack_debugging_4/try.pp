cd4bf6fada96be920fc068eddaaf634bbc2a0eba
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-04-20 02:53:40 +0000

    removed a trial file

--- 0x1B-web_stack_debugging_4/try.pp
+++
@@ -1,6 +0,0 @@
-# Puppet manifest to change OS configuration for the holberton user
-
-exec { 'increase_open_files_limit':
-  command => '/bin/bash -c "ulimit -n 10000"',
-  user    => 'holberton',
-}
